a58fb248fe377aa6ce4f78b4c3bd9fc91b2d2998
brooktewabe <brooktewabe00@gmail.com>
2024-09-24 15:19:44 +0300

    'demo'

--- src/axiosInterceptor.jsx
+++ src/axiosInterceptor.jsx
@@ -5,8 +5,8 @@ axios.interceptors.request.use(
   (config) => {
     const token = Cookies.get('jwt');
     if (token) {
-      // config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0OTA1YzgyLTdlY2MtNDVjZS1hNTMyLTJlYTNlN2YzYzY0NCIsImlhdCI6MTcyNzA4OTAzNSwiZXhwIjoxNzI3MTEwNjM1fQ.jEBzXW8iz1p9X_QIXR04csL2e3elkUn4NPBbNjOA9zs`;
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA0OTA1YzgyLTdlY2MtNDVjZS1hNTMyLTJlYTNlN2YzYzY0NCIsImlhdCI6MTcyNzA4OTAzNSwiZXhwIjoxNzI3MTEwNjM1fQ.jEBzXW8iz1p9X_QIXR04csL2e3elkUn4NPBbNjOA9zs`;
+      // config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
--- src/withAuth.jsx
+++ src/withAuth.jsx
@@ -13,25 +13,25 @@ const withAuth = (WrappedComponent) => {
       const [redirecting, setRedirecting] = useState(false);
       const cookies = new Cookies(); 
 
-      useEffect(() => {
-        const checkAuth = async () => {
-            const jwtToken = cookies.get('jwt');
-            if (!jwtToken) {
-                setRedirecting(true);
-                navigate('/login');
-            } else {
-                setLoading(false);
-                setUser(true);
-            }
-        };
+      // useEffect(() => {
+      //   const checkAuth = async () => {
+      //       const jwtToken = cookies.get('jwt');
+      //       if (!jwtToken) {
+      //           setRedirecting(true);
+      //           navigate('/login');
+      //       } else {
+      //           setLoading(false);
+      //           setUser(true);
+      //       }
+      //   };
         
-        checkAuth();
-      }, [navigate, cookies]);
+      //   checkAuth();
+      // }, [navigate, cookies]);
 
 
-      if (loading || redirecting) {
-        return <div><Spinner /></div>;
-      }
+      // if (loading || redirecting) {
+      //   return <div><Spinner /></div>;
+      // }
   
       return <WrappedComponent {...props} />;
     };
