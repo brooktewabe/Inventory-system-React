3a938772a360165d7a5c100972c943f27598182c
brooktewabe <brooktewabe00@gmail.com>
2024-09-25 10:00:00 +0300

    '404-fix'

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://system.tridal.org',
   plugins: [react()],
   server: {
     port: 3000,
@@ -15,4 +16,7 @@ export default defineConfig({
       },
     },
   },
+  resolve:{
+    alias:[{ find: "@", replacement: "/src"}]
+  }
 })
